5c754582388f839b79653d902bcbe4d4bd36f680
Corey Samuels <coreyjsamuels@gmail.com>
2020-12-19 01:40:03 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
-const { sequelize, DataTypes, Sequelize } = require("sequelize");
 const mysql = require("mysql");
 const ck = require("ckey");
 const password = ck.password;
-require("dotenv").config();
-const DATABASE_URL = ck.DATABASE_URL;
+const PORT = ck.PORT;
 const connection = mysql.createConnection({
   host: "n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
   user: "ue6j2q6gugp5ggfu",
   password: password,
   database: "burgersDB",
-  port: process.env.PORT,
+  port: PORT,
 });
 
 module.exports = connection;
