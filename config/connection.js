cf09d895c1e6f7c1570dba1c7c1f579539ee64ca
Corey Samuels <coreyjsamuels@gmail.com>
2020-12-19 01:47:51 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,7 @@ const ck = require("ckey");
 const password = ck.password;
 const PORT = ck.PORT;
 const connection = mysql.createConnection({
-  host: "n2o93bb1bwmn0zle.chr7pe7iynqr.eu-west-1.rds.amazonaws.com",
-  user: "ue6j2q6gugp5ggfu",
   password: password,
-  port: PORT,
 });
 
 module.exports = connection;
