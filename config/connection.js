75bef6685ffdbf5fe98e2e5c3c49fdbfa851f641
Corey Samuels <coreyjsamuels@gmail.com>
2020-12-19 01:51:47 +0000

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,6 @@ const mysql = require("mysql");
 const ck = require("ckey");
 const password = ck.password;
 const PORT = ck.PORT;
-const connection = mysql.createConnection({
-  password: password,
-});
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 module.exports = connection;
